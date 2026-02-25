97b7aed07b71cbf9c0531e59627b90359559bae3
Bodasu karunanjali <karunanjalibodas13@gmail.com>
2026-02-25 15:55:57 +0530

    Update Bulk_Prediction.py

--- pages/Bulk_Prediction.py
+++ pages/Bulk_Prediction.py
@@ -11,11 +11,14 @@ file = st.file_uploader("Upload CSV")
 if file:
     df = pd.read_csv(file)
 
+    # ⭐ get model feature list
     cols = model.get_booster().feature_names
-    df = df.reindex(columns=cols, fill_value=0)
 
-    df['churn_prob'] = model.predict_proba(df)[:,1]
-    df['prediction'] = model.predict(df)
+    # ⭐ create model input dataframe
+    df_model = df.reindex(columns=cols, fill_value=0)
+
+    # ⭐ predictions using df_model (IMPORTANT)
+    df["churn_prob"] = model.predict_proba(df_model)[:,1]
+    df["prediction"] = model.predict(df_model)
 
     st.write(df.head())
-    
